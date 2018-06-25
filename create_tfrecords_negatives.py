51d3db8a054fd23af04b91c06138597dda3f77fc
Ruben Tous <ruben.tous@adsmurai.com>
2018-06-25 15:13:40 +0200

    nc

---
+++ create_tfrecords_negatives.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -------------------------------------------------------------------
+# File Name : create_dataset_events.py
+# Creation Date : 05-12-2016
+# Last Modified : Fri Jan  6 15:04:54 2017
+# Author: Thibaut Perol <tperol@g.harvard.edu>
+# -------------------------------------------------------------------
+"""Creates tfrecords dataset of events trace and their cluster_ids.
+This is done by loading a dir of .mseed and one catalog with the
+time stamps of the events and their cluster_id
+e.g.,
+./bin/preprocess/create_dataset_events.py \
+--stream_dir data/streams \
+--catalog data/50_clusters/catalog_with_cluster_ids.csv\
+--output_dir data/50_clusters/tfrecords
+"""
+
+import os
+import numpy as np
+from quakenet.data_pipeline import DataWriter
+import tensorflow as tf
+from obspy.core import read
+from quakenet.data_io import load_catalog
+from obspy.core.utcdatetime import UTCDateTime
+from openquake.hazardlib.geo.geodetic import distance
+import fnmatch
+import json
+
+INPUT_STREAM_DIR = "funvisis/funvisis2oklahoma/mseed_noise"
+OUTPUT_TFRECORDS_DIR = "output_funvisis/negative"
+WINDOW_SIZE = 10
+
+def preprocess_stream(stream):
+    stream = stream.detrend('constant')
+    return stream.normalize()
+
+def main(_):
+
+    stream_files = [file for file in os.listdir(INPUT_STREAM_DIR) if
+                    fnmatch.fnmatch(file, '*.mseed')]
+
+    # Create dir to store tfrecords
+    if not os.path.exists(OUTPUT_TFRECORDS_DIR):
+        os.makedirs(OUTPUT_TFRECORDS_DIR)
+
+    # Write event waveforms and cluster_id in .tfrecords
+    output_name = "negatives.tfrecords"
+    output_path = os.path.join(OUTPUT_TFRECORDS_DIR, output_name)
+    writer = DataWriter(output_path)
+    for stream_file in stream_files:
+        stream_path = os.path.join(INPUT_STREAM_DIR, stream_file)
+        print "+ Loading Stream {}".format(stream_file)
+        st_event = read(stream_path)
+        print '+ Preprocessing stream'
+        st_event = preprocess_stream(st_event)
+
+        #cluster_id = filtered_catalog.cluster_id.values[event_n]
+        cluster_id = -1 #We work with only one location for the moment (cluster id = 0)
+        n_traces = len(st_event)
+        # If there is not trace skip this waveform
+        if n_traces == 0:
+            continue
+        n_samples = len(st_event[0].data)
+        n_pts = st_event[0].stats.sampling_rate * WINDOW_SIZE + 1
+        if (len(st_event) == 3) and (n_pts == n_samples) and (st_event[0].stats.sampling_rate == 100.0):
+            print("Writing sample with dimensions "+str(WINDOW_SIZE)+"x"+str(st_event[0].stats.sampling_rate)+"x"+str(n_traces))
+            # Write tfrecords
+            writer.write(st_event, cluster_id) 
+        else:
+            print ("\033[91m WARNING!!\033[0m Missing waveform for event in "+stream_file)
+
+    # Cleanup writer
+    print("Number of events written={}".format(writer._written))
+    writer.close()
+    
+
+if __name__ == "__main__":
+    tf.app.run()
--- funvisis2oklahoma.py
+++ funvisis2oklahoma.py
@@ -39,8 +39,14 @@ import seisobs #https://github.com/d-chambers/seisobs
 INPUT_STREAM_DIR = "funvisis/mseed"
 INPUT_METADATA_DIR = "funvisis/sfiles_nordicformat"
 OUTPUT_MSEED_DIR = "funvisis/funvisis2oklahoma/mseed"
+OUTPUT_MSEED_EVENT_DIR = "funvisis/funvisis2oklahoma/mseed_10s"
+OUTPUT_MSEED_NOISE_DIR = "funvisis/funvisis2oklahoma/mseed_noise"
 OUTPUT_PNG_DIR = "funvisis/funvisis2oklahoma/png"
+OUTPUT_PNG_EVENT_DIR = "funvisis/funvisis2oklahoma/png_10s"
+OUTPUT_PNG_NOISE_DIR = "funvisis/funvisis2oklahoma/png_noise"
 WINDOW_SIZE = 10
+WINDOW_STEP_NEGATIVES = 10
+WINDOW_AVOID_NEGATIVES = 20
 
 def preprocess_stream(stream):
     stream = stream.detrend('constant')
@@ -63,6 +69,14 @@ def main(args):
         os.makedirs(OUTPUT_MSEED_DIR)
     if not os.path.exists(OUTPUT_PNG_DIR):
         os.makedirs(OUTPUT_PNG_DIR)
+    if not os.path.exists(OUTPUT_MSEED_EVENT_DIR):
+        os.makedirs(OUTPUT_MSEED_EVENT_DIR)
+    if not os.path.exists(OUTPUT_PNG_EVENT_DIR):
+        os.makedirs(OUTPUT_PNG_EVENT_DIR)
+    if not os.path.exists(OUTPUT_MSEED_NOISE_DIR):
+        os.makedirs(OUTPUT_MSEED_NOISE_DIR)
+    if not os.path.exists(OUTPUT_PNG_NOISE_DIR):
+        os.makedirs(OUTPUT_PNG_NOISE_DIR)
 
     metadata_files = [file for file in os.listdir(INPUT_METADATA_DIR) if
                     fnmatch.fnmatch(file, '*')]
@@ -116,9 +130,29 @@ def processMseed(stream_file, obspyCatalogMeta):
                 timeP = pick.time
                 print("P time = "+str(timeP))
                 win = substream.slice(UTCDateTime(timeP), UTCDateTime(timeP) + WINDOW_SIZE).copy()
-                win.plot(outfile=OUTPUT_PNG_DIR+"_10s/"+stream_file+"_"+station_code+".png")
-                print ("Saving file "+OUTPUT_MSEED_DIR+"_10s/"+stream_file+"_"+station_code+".mseed")
-                win.write(OUTPUT_MSEED_DIR+"_10s/"+stream_file+"_"+station_code+".mseed", format="MSEED") 
+                win.plot(outfile=OUTPUT_PNG_EVENT_DIR+"/"+stream_file+"_"+station_code+".png")
+                print ("Saving file "+OUTPUT_MSEED_EVENT_DIR+"/"+stream_file+"_"+station_code+".mseed")
+                win.write(OUTPUT_MSEED_EVENT_DIR+"/"+stream_file+"_"+station_code+".mseed", format="MSEED") 
+
+                #Save noise windows (exclude from P wave and 20s later)
+                win_gen = substream.slide(window_length=WINDOW_SIZE,
+                           step=WINDOW_STEP_NEGATIVES,
+                           include_partial_windows=False)
+                total_time = substream[0].stats.endtime - substream[0].stats.starttime
+                max_windows = (total_time - WINDOW_SIZE) / WINDOW_STEP_NEGATIVES
+                for idx, win in enumerate(win_gen):
+                    window_start = win[0].stats.starttime.timestamp
+                    window_end = win[-1].stats.endtime.timestamp
+                    if ((window_start < timeP and window_end < timeP)
+                        or
+                        (window_start > timeP+WINDOW_AVOID_NEGATIVES)
+                        ):
+                        print("Noise window selected")
+                        print ("Saving file "+OUTPUT_MSEED_NOISE_DIR+"/"+stream_file+"_"+station_code+"_noise"+str(idx)+".mseed")
+                        win.write(OUTPUT_MSEED_NOISE_DIR+"/"+stream_file+"_"+station_code+"_noise"+str(idx)+".mseed", format="MSEED") 
+                        win.plot(outfile=OUTPUT_PNG_NOISE_DIR+"/"+stream_file+"_"+station_code+"_noise"+str(idx)+".png")
+                    else:
+                        print("Noise window avoided: "+stream_file+"_"+station_code)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
