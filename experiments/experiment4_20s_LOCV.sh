3adea0b85da825072cb7848c625267dbde924a82
Ruben Tous <rtous@dync-34-230.ac.upc.es>
2018-07-26 14:58:43 +0200

    nc

--- experiments/experiment4_20s_LOCV.sh
+++ experiments/experiment4_20s_LOCV.sh
@@ -2,42 +2,42 @@
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0420-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords1 \
 --file_name 2015-02-05-0420-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0420-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords1 \
 --file_name 2015-02-05-0420-00S.tfrecords
 
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0538-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords1 \
 --file_name 2015-02-05-0538-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0538-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords1 \
 --file_name 2015-02-05-0538-00S.tfrecords
 
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0703-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords1 \
 --file_name 2015-02-05-0703-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0703-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords1 \
 --file_name 2015-02-05-0703-00S.tfrecords
 
@@ -57,42 +57,42 @@ python step5_eval.py \
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0420-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords2 \
 --file_name 2015-02-05-0420-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0420-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords2 \
 --file_name 2015-02-05-0420-00S.tfrecords
 
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0538-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords2 \
 --file_name 2015-02-05-0538-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0538-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords2 \
 --file_name 2015-02-05-0538-00S.tfrecords
 
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-01-10-0517-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords2 \
 --file_name 2015-01-10-0517-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-01-10-0517-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords2 \
 --file_name 2015-01-10-0517-00S.tfrecords
 
@@ -112,42 +112,42 @@ python step5_eval.py \
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0420-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords3 \
 --file_name 2015-02-05-0420-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0420-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords3 \
 --file_name 2015-02-05-0420-00S.tfrecords
 
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0703-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords3 \
 --file_name 2015-02-05-0703-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0703-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords3 \
 --file_name 2015-02-05-0703-00S.tfrecords
 
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-01-10-0517-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords3 \
 --file_name 2015-01-10-0517-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-01-10-0517-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords3 \
 --file_name 2015-01-10-0517-00S.tfrecords
 
@@ -168,42 +168,42 @@ python step5_eval.py \
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0538-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords4 \
 --file_name 2015-02-05-0538-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0538-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords4 \
 --file_name 2015-02-05-0538-00S.tfrecords
 
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0703-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords4 \
 --file_name 2015-02-05-0703-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-02-05-0703-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords4 \
 --file_name 2015-02-05-0703-00S.tfrecords
 
 python step2_preprocess2_create_tfrecords_positives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-01-10-0517-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords4 \
 --file_name 2015-01-10-0517-00S.tfrecords
 
 python step3_preprocess3_create_tfrecords_negatives.py \
 --config_file_path experiments/config_20s.ini \
 --pattern 2015-01-10-0517-00S*.mseed \
---prep_data_dir output/data_prep_experiment4_20s \
+--prep_data_dir output/data_prep_experiment3_20s \
 --tfrecords_dir output/data_prep_experiment4_20s_LOCV/tfrecords4 \
 --file_name 2015-01-10-0517-00S.tfrecords
 
