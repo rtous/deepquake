5aada61cb82462639105446e0fde4cd24928be0b
Ruben Tous <rtous@dync-34-230.ac.upc.es>
2018-10-11 10:13:55 +0200

    nc

---
+++ experiments/experiment_50s_olddata_oldmodel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+EXPERIMENT_NAME=50s_olddata_oldmodel
+INPUT_DATA_DIR=datos1
+
+CONFIG_FILE=experiments/config_50s.ini
+DATA_PREP_DIR=data_prep_50s_olddata
+DATA_TRAIN_DIR=train_50s_olddata_oldmodel
+
+python step4_train.py \
+--config_file_path $CONFIG_FILE \
+--tfrecords_dir output/$DATA_PREP_DIR/tfrecords \
+--checkpoint_dir output/$DATA_TRAIN_DIR/checkpoints
+
+python step5_eval_over_tfrecords.py \
+--config_file_path $CONFIG_FILE \
+--checkpoint_dir output/$DATA_TRAIN_DIR/checkpoints \
+--output_dir output/$DATA_TRAIN_DIR/eval \
+--tfrecords_dir output/$DATA_PREP_DIR/tfrecords/test
