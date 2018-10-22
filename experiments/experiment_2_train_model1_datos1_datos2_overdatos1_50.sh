217a93a2a82b41a9601c4d7f56c99d85a111d979
Ruben Tous <rtous@dync-34-230.ac.upc.es>
2018-10-22 10:29:23 +0200

    nc

---
+++ experiments/experiment_2_train_model1_datos1_datos2_overdatos1_50.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TRAIN_CONFIG=train_default
+
+###################################
+# 1. DATOS 1 y 2 OVER DATOS 1 
+################################### 
+INPUT_DATA_DIR=datos1_datos2
+
+# 50s
+WINDOW_SIZE=50
+DATA_PREP_MAIN_DIR_NAME=data_prep_$INPUT_DATA_DIR
+DATA_PREP_DIR=$DATA_PREP_MAIN_DIR_NAME/$WINDOW_SIZE
+DATA_TRAIN_DIR=$DATA_PREP_DIR/$TRAIN_CONFIG
+
+python step5_eval_over_tfrecords.py \
+--component_N 0 \
+--component_E 0 \
+--window_size $WINDOW_SIZE \
+--checkpoint_dir output/$DATA_TRAIN_DIR/checkpoints \
+--output_dir output/$DATA_TRAIN_DIR/eval_overdatos1 \
+--tfrecords_dir output/data_prep_datos1/$WINDOW_SIZE/Z/tfrecords/test
+
