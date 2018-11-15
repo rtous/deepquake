9ad6b9c28ce826f7e4c9fb9240d9e0f20da516a9
Ruben Tous <ruben.tous@adsmurai.com>
2018-11-15 19:30:16 +0100

    nc

--- experiments/launch_batch.sh
+++ experiments/launch_batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./experiments/launch.sh ./experiments/experiment_6_train.sh K3 data1 10
-./experiments/launch.sh ./experiments/experiment_6_train.sh K3 data1 20
+./experiments/launch.sh ./experiments/experiment_6_train.sh K3 datos1 10
+#./experiments/launch.sh ./experiments/experiment_6_train.sh K3 datos1 20
 
 
 
